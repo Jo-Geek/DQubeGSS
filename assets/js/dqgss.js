1fd674893e0b2f2219f7ac102439bd2d61cf7023
Jo-Geek <jonesvinothjoseph@hotmail.com>
2019-01-12 11:36:54 +0530

    Scroll Action

--- assets/js/dqgss.js
+++ assets/js/dqgss.js
@@ -1,7 +1,36 @@
 $(document).ready(function () {
-
   $('.spinner').addClass('activate');
 })
+
+var lastScrollTop = 0;
+var scrollInAction = false;
+$(window).scroll(function (event) {
+  var st = $(this).scrollTop();
+  
+  if (scrollInAction) { return; }
+
+  if (st > lastScrollTop) {
+    // downscroll code
+
+    if ($(window).scrollTop() < $('main').position().top) {
+      scrollInAction = true;
+      $('html').animate({ scrollTop: $('main').position().top }, 400, 'swing', function () {
+        scrollInAction = false;
+      });
+    }
+
+  } else {
+    // upscroll code
+
+    if ($(window).scrollTop() < $('main').position().top) {
+      scrollInAction = true;
+      $('html').animate({ scrollTop: 0 }, 400, 'swing', function () {
+        scrollInAction = false;
+      });
+    }
+  }
+  lastScrollTop = st;
+});
 
 window.onload = function () {
 
